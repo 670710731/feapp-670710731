085abd5777476da74c45bb91f1b2275b7abad5bd
phanukon phimpha <phimpa_p@su.ac.th>
2026-09-10 04:19:10 +0000

    Add Image Black CLover

--- phanukon-app/src/data.js
+++ phanukon-app/src/data.js
@@ -120,6 +120,6 @@ export const movies = [
     "year": 2026,
     "rating": 7.2,
     "detail": "เรื่องราวของเด็กหนุ่มที่เกิดมาพร้อมกับเวทมนตร์ แต่กลับไม่มีเวทมนตร์เลยสักนิดเดียว เขาและเพื่อนสนิทของเขาได้ออกเดินทางเพื่อที่จะเป็นจอมเวทที่ยิ่งใหญ่ที่สุดในโลก",
-    "poster": "https://image.tmdb.org/t/p/w342/uxCaBoYXsDC4A0SqTm3SISj0OwK.jpg"
+    "poster": "https://ibb.co/JWPBDknj"
   }
 ];
